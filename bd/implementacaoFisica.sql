b8ee63073083dfbf25d88fd7da25541ec24f6b8f
a104360 <a104360@alunos.uminho.pt>
2024-05-29 09:27:16 +0100

    commit finnal projeto bd

--- bd/implementacaoFisica.sql
+++ bd/implementacaoFisica.sql
@@ -10,7 +10,7 @@ idPagamento int not null auto_increment,
 estado boolean not null,
 valor double not null,
 metodo int not null,
-data date not null,
+data date,
 primary key (idPagamento)
 );
 
@@ -115,3 +115,137 @@ create table if not exists TecnicaDoPedido (
     foreign key (pedido) references Pedido(idPedido),
     foreign key (tecnica) references Tecnica(idTecnica)
 );
+
+-- Funcao para calcular as despesas totais de um pedido
+DELIMITER $$
+CREATE FUNCTION calcularDespesaTotal(pedidoId INT) 
+	RETURNS DOUBLE
+	DETERMINISTIC
+	BEGIN
+		DECLARE total DOUBLE;
+
+		SELECT (d.bonus + d.alimentacao + d.alojamento + d.equipamento + d.outros) INTO total
+		FROM Pedido p
+		INNER JOIN Despesa d ON p.idDespesa = d.idDespesa
+		WHERE p.idPedido = pedidoId;
+
+		RETURN total;
+	END $$
+
+
+
+
+-- Querie1 agentes que nao estao a associados a pedidos
+delimiter $$
+CREATE PROCEDURE agentesLivres()
+	BEGIN
+		SELECT idFuncionario AS Agentes FROM Funcionario 
+			WHERE Funcionario.departamento LIKE 'Inv%'
+			AND Funcionario.idFuncionario NOT IN (
+				SELECT idAgente FROM AgentesDoPedido 
+				JOIN Pedido ON AgentesDoPedido.idPedido = Pedido.idPedido 
+				WHERE Pedido.status = 0
+			);
+END $$
+
+    
+    
+    
+-- Querie2 pedidos que estao pendentes
+delimiter $$
+CREATE PROCEDURE pedidosPendentes()
+	BEGIN
+		SELECT idPedido FROM Pedido
+		WHERE Pedido.status = 0;
+	END $$
+    
+    
+    
+    
+-- Querie3 ver quanto se gastou em equipamentos num pedido
+delimiter $$
+CREATE PROCEDURE gastoEquipamentosPedido(IN idP INT)
+	BEGIN
+		SELECT equipamento AS gastoEquipamento FROM Despesa 
+        JOIN Pedido ON Pedido.idPedido = idP
+			WHERE Despesa.idDespesa = Pedido.idDespesa;
+    END $$
+
+
+
+
+-- Querie4 todos os pedidos associados a um cliente
+delimiter $$
+CREATE PROCEDURE pedidosCliente(IN idC INT)
+	BEGIN
+		SELECT Pedido.idPedido, Pedido.nivel, Pedido.relatorio, Pedido.status, Pedido.inicio, Pedido.fim, Pagamento.estado AS estado_Pagamento, Cliente.nome AS nome_Cliente, Agencia.codigoPostal AS codigoPostal_Agencia FROM Pedido
+			INNER JOIN Pagamento ON Pagamento.idPagamento = Pedido.idPagamento INNER JOIN Cliente ON Cliente.idCliente = Pedido.idCliente INNER JOIN Agencia ON Agencia.idAgencia = Pedido.idAgencia
+			WHERE Pedido.idCliente = idC;
+	END $$
+
+
+
+
+-- TRIGGER Verificar se a data de inicio de um pedido vem antes da data de fim
+DELIMITER $$
+CREATE TRIGGER verificaDataPedido
+	BEFORE UPDATE ON Pedido
+	FOR EACH ROW
+	BEGIN
+		IF NEW.inicio > NEW.fim THEN
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'A data de início é posterior à data de fim'; -- 45000 Erro defenido pelo usuário
+		END IF;
+	END $$
+
+
+
+-- PROCEDURE
+DELIMITER $$
+CREATE PROCEDURE atualizarStatusPedido(IN pedidoId INT, IN novoStatus INT, IN dataFim DATE)
+	BEGIN
+		START TRANSACTION;
+		
+        -- Atualiza o status do pedido e a data de fim
+		UPDATE Pedido
+		SET status = novoStatus, fim = dataFim
+		WHERE idPedido = pedidoId;
+
+		-- Verifica se o pedido foi atualizado
+		IF ROW_COUNT() = 0 THEN
+			ROLLBACK;
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Pedido não encontrado ou atualização falhou';
+		ELSE
+			COMMIT;
+		END IF;
+	END $$
+
+
+
+
+-- VIEW para ver os detalhes de um pedido
+CREATE VIEW DetalhesPedido AS
+	SELECT 
+		p.idPedido, p.nivel, p.tipo, p.relatorio, p.status, p.inicio, p.fim,
+		pg.estado AS estado_pagamento, c.nome AS nome_cliente, a.email AS email_agencia,
+        calcularDespesaTotal(p.idPedido) AS despesa
+		FROM Pedido p
+		INNER JOIN Pagamento pg ON p.idPagamento = pg.idPagamento
+		INNER JOIN Cliente c ON p.idCliente = c.idCliente
+		INNER JOIN Agencia a ON p.idAgencia = a.idAgencia
+		INNER JOIN Despesa d ON p.idDespesa = d.idDespesa;
+
+
+
+-- VIEW Ids dos pedidos com os agentes associados
+CREATE VIEW AgentesPorPedido AS
+	SELECT AgentesDoPedido.idPedido, Funcionario.nome AS nome_agente
+	FROM AgentesDoPedido
+	INNER JOIN Funcionario ON AgentesDoPedido.idAgente = Funcionario.idFuncionario;
+
+
+
+
+-- VIEW Todos os funcionarios ordenados por id agencia
+CREATE VIEW FuncionariosPorAgencia AS
+	SELECT * FROM Funcionario
+		ORDER BY agencia;
--- bd/povoamento.sql
+++ bd/povoamento.sql
@@ -1,16 +1,33 @@
 -- Exemplos para a tabela Pagamento
 INSERT INTO Pagamento (estado, valor, metodo, data) VALUES 
-(true, 150.00, 1, '2024-01-15'),
-(false, 200.00, 2, '2024-02-20'),
-(true, 300.50, 1, '2024-03-10'),
-(false, 450.00, 3, '2024-04-05');
+(true, 1500.00, 1, '2024-05-01'),
+(false, 550.00, 2, null), 
+(true, 430.00, 2, '2024-05-02'),
+(true, 600.00, 1, '2024-05-03'),
+(false, 700.00, 3, null),
+(true, 2000.00, 2, '2024-07-01'),
+(true, 400.00, 1, '2024-08-20'),
+(false, 350.00, 2, null), 
+(true, 450.00, 2, '2024-10-25'),
+(true, 550.00, 1, '2024-11-02'),
+(true, 500.00, 3, '2024-12-05'),
+(true, 750.00, 1, '2025-01-03');
 
 -- Exemplos para a tabela Despesa
 INSERT INTO Despesa (bonus, alimentacao, alojamento, equipamento, outros) VALUES 
-(100, 50, 150, 200, 100),
-(150, 60, 180, 220, 120),
-(120, 70, 160, 180, 110),
-(200, 80, 200, 250, 150);
+(100, 50, 100, 50, 50),
+(0, 100, 0, 0, 0),
+(100, 50, 100, 50, 0),
+(200, 50, 100, 100, 50),
+(0, 100, 0, 0, 0),
+(200, 50, 100, 50, 0),
+(100, 50, 100, 50, 0),
+(0, 100, 0, 0, 0),
+(200, 50, 100, 50, 0),
+(250, 100, 100, 50, 0),
+(0, 100, 0, 0, 0),
+(250, 100, 100, 50, 0);
+
 
 -- Exemplos para a tabela Localidade
 INSERT INTO Localidade (nome) VALUES 
@@ -23,39 +40,65 @@ INSERT INTO Localidade (nome) VALUES
 
 -- Exemplos para a tabela Agencia
 INSERT INTO Agencia (email, numero, porta, localidade, codigoPostal) VALUES 
-('braga@iap.pt', 253303030, 10, 3, 4719006);
+('braga@iap.pt', 253303030, 10, 3, 4719006),
+('lisboa@iap.pt', 253236421, 15, 1, 1346007);
 
 -- Exemplos para a tabela Funcionario
 INSERT INTO Funcionario (nome, salario, inicioContrato, fimContrato, departamento, posicao, dataNascimento, porta, localidade, codigoPostal, numero, email, agencia) VALUES 
 ('Aristides Piratininga', 2500.00, '2022-05-20', NULL, 'Administração', 'CEO', '1986-12-01', 13, 3, 4730301, 910934415, 'aristides.obi@investigacao.pt', 1),
 ('Laura Hipólito', 2500.00, '2022-05-25', NULL, 'Equipa jurídica', 'Advogada', '2000-04-25', 34, 6, 4730203, 664115266, 'hipolito@investigacao.pt', 1),
-('João Freitas', 2450.00, '2022-06-14', NULL, 'Finanças', 'Gestor Sénior', '2003-11-15', 56, 3, 4715001, 967566234, 'joao.freitas@investigacao.pt', 1),
-('Maria Costa', 700.00, '2024-01-15', NULL, 'Investigação', 'Agente júnior', '1999-03-30', 78, 3, 4720313, 913141250, 'maria.costa@investigacao.pt', 1),
-('Marco Costa', 700.00, '2024-01-15', NULL, 'Investigação', 'Agente júnior', '2004-10-10', 3, 3, 4702712, 917771322, 'marco.tosta@investigacao.pt', 1);
+('João Freitas', 2450.00, '2022-06-14', NULL, 'Finanças', 'Gestor Sénior', '2003-11-15', 56, 2, 4715001, 967566234, 'joao.freitas@investigacao.pt', 1),
+('Maria Costa', 700.00, '2024-01-15', NULL, 'Investigação', 'Agente sénior', '1999-03-30', 78, 4, 4720313, 913141250, 'maria.costa@investigacao.pt', 1),
+('Marco Costa', 700.00, '2024-01-15', NULL, 'Investigação', 'Agente júnior', '2004-10-10', 3, 5, 4702712, 917771322, 'marco.tosta@investigacao.pt', 1),
+('Ana Silva', 1500.00, '2023-01-10', '2024-01-10', 'Finanças', 'Contabilista', '1980-07-22', 45, 1, 4725301, 912345678, 'ana.silva@investigacao.pt', 2),
+('Rui Dias', 1300.00, '2023-02-01', '2024-02-01', 'Finanças', 'Assistente Financeiro', '1992-02-13', 50, 2, 4720302, 912345679, 'rui.dias@investigacao.pt', 1),
+('Inês Marques', 900.00, '2023-03-01', NULL, 'Investigação', 'Agente', '1995-05-10', 21, 3, 4710303, 912345680, 'ines.marques@investigacao.pt', 2),
+('Pedro Almeida', 950.00, '2023-04-15', '2024-04-15', 'Investigação', 'Agente', '1998-08-22', 15, 1, 4705311, 912345681, 'pedro.almeida@investigacao.pt', 2),
+('Sara Fernandes', 1200.00, '2023-06-10', NULL, 'Equipa jurídica', 'Advogada júnior', '1997-12-05', 30, 1, 4720402, 912345682, 'sara.fernandes@investigacao.pt', 2),
+('Luís Gomes', 1300.00, '2023-07-01', '2024-07-01', 'Finanças', 'Analista', '1990-10-14', 25, 4, 4710304, 912345683, 'luis.gomes@investigacao.pt', 2),
+('Beatriz Silva', 1400.00, '2023-08-05', NULL, 'Administração', 'Assistente', '1985-11-20', 18, 5, 4725302, 912345684, 'beatriz.silva@investigacao.pt', 2);
 
 -- Exemplos para a tabela Cliente
 INSERT INTO Cliente (nome, registoCriminal, NIF, porta, localidade, codigoPostal, numero, email) VALUES 
 ('Carlos Almeida', 'Sem registo', 123456789, 5, 1, 1100, 987654321, 'carlos.almeida@cliente.pt'),
 ('Sofia Carvalho', 'Sem registo', 987654321, 15, 2, 4100, 876543210, 'sofia.carvalho@cliente.pt'),
 ('Rui Martins', 'Registo de pequeno delito', 112233445, 25, 3, 8000, 765432109, 'rui.martins@cliente.pt'),
-('Beatriz Oliveira', 'Sem registo', 556677889, 35, 4, 3000, 654321098, 'beatriz.oliveira@cliente.pt');
+('Beatriz Oliveira', 'Sem registo', 556677889, 35, 4, 3000, 654321098, 'beatriz.oliveira@cliente.pt'),
+('João Silva', 'Sem registo', 998877665, 45, 5, 2000, 543210987, 'joao.silva@cliente.pt'),
+('Marta Santos', 'Sem registo', 334455667, 55, 6, 9000, 432109876, 'marta.santos@cliente.pt');
+
 
 -- Exemplos para a tabela Pedido
 INSERT INTO Pedido (nivel, tipo, relatorio, status, inicio, fim, idPagamento, idCliente, idAgencia, idDespesa) VALUES 
-('Regional', 'Investigação', 'Relatório completo', 1, '2024-02-01', '2024-02-28', 1, 1, 1, 1),
-('Regional', 'Monitorização', 'Relatório parcial', 0, '2024-03-01', '2024-03-20', 2, 2, 1, 2),
-('Local', 'Investigação', 'Relatório consultoria', 1, '2024-04-01', '2024-04-15', 3, 3, 1, 3),
-('Nacional', 'Investigação', 'Relatório final', 1, '2024-05-01', '2024-05-30', 4, 4, 1, 4);
+('Regional', 'Investigação', 'Relatório completo: ...', 1, '2024-02-01', '2024-02-28', 1, 1, 1, 1),
+('Local', 'Monitorização', 'Relatório parcial: ...', 0, '2024-03-01', null, 2, 2, 2, 2),
+('Local', 'Investigação', 'Relatório consultoria: ...', 1, '2024-04-01', '2024-04-15', 3, 3, 2, 3),
+('Nacional', 'Investigação', 'Relatório final: ...', 1, '2024-05-01', '2024-05-30', 4, 4, 1, 4),
+('Local', 'Menores', 'Relatório inicial: ...', 0, '2024-06-01', null, 5, 5, 2, 5),
+('Internacional', 'Infidelidade', 'Relatório detalhado: ...', 1, '2024-07-01', '2024-07-31', 6, 6, 1, 6),
+('Local', 'Investigação', 'Relatório de progresso: ...', 1, '2024-08-01', '2024-08-20', 7, 4, 1, 7),
+('Local', 'Monitorização', 'Relatório sumário: ...', 0, '2024-09-01', null, 8, 2, 1, 8),
+('Nacional', 'Menores', 'Relatório final: ...', 1, '2024-10-01', '2024-10-25', 9, 5, 2, 9),
+('Governamental', 'Infidelidade', 'Relatório preliminar: ...', 1, '2024-11-01', '2024-11-30', 10, 3, 2, 10),
+('Local', 'Monitorização', 'Relatório interino: ...', 1, '2024-12-01', '2024-12-05', 11, 4, 1, 11),
+('Local', 'Infidelidade', 'Relatório de conclusão: ...', 1, '2024-12-15', '2024-12-31', 12, 1, 1, 12);
 
-
--- Exemplos para a tabela AgentesDoPedido
 INSERT INTO AgentesDoPedido (idPedido, idAgente) VALUES 
-(1, 1),
-(1, 2),
-(2, 2),
-(3, 3),
+(1, 4),
+(1, 5),
+(2, 9),
+(3, 8),
 (4, 4),
-(4, 5);
+(4, 5),
+(5, 8),
+(6, 4),
+(7, 5),
+(8, 5),
+(9, 9),
+(10, 9),
+(11, 4),
+(12, 4);
+
 
 -- Exemplos para a tabela Tecnica
 INSERT INTO Tecnica (nome) VALUES 
@@ -72,4 +115,17 @@ INSERT INTO TecnicaDoPedido (pedido, tecnica) VALUES
 (2, 2),
 (3, 3),
 (3, 1),
-(4, 4);
+(4, 4),
+(5, 1),
+(5, 3),
+(6, 2),
+(7, 1),
+(7, 4),
+(8, 2),
+(9, 3),
+(9, 4),
+(10, 1),
+(11, 2),
+(11, 3),
+(12, 4),
+(12, 1);
